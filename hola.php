b5379efa302da1ee47ccbaf4e05be2bcfb2ecf14
Cristian Sanchez <csb@email.com>
2023-05-15 07:58:01 +0000

    Añadida la etiqueta de autor y el email

--- hola.php
+++ hola.php
@@ -1,5 +1,5 @@
 <?php
-// Cristian Sanchez Barba
+// Autor: Cristian Sanchez Barba <mail@mail.com>
 // El nombre por defecto es Mundo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
